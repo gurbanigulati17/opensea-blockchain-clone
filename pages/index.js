b3e7840153ff4aaf2625e42d2c6ea3d6f5378c60
Gurubani Gulati <gurbanigulati17@gmail.com>
2022-07-10 16:00:34 +0530

    Home component completed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 
 import Header from "../components/Header"
-
+import Hero from "../components/Hero"
 
 export default function Home() {
 
-    return <Header />
+    return (
+        <>
+        <Header />
+        <Hero />
+    </>
+
+    )
+  
    
 };
 
